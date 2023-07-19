0f5b5885fb70ce93ea4736eed4dab4a9d9d1d658
karanja03 <wambuik03@gmail.com>
2023-07-19 11:47:57 +0300

    Complete hooks event handler

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,15 @@
 // Code EyesOnMe Component Here
+import React from "react";
+function EyesOnMe(){
+
+    const handleFocus=()=>{
+        console.log("Good!");
+    }
+    const handleBlur=()=>{
+        console.log("Hey! Eyes On Me!")
+    }
+    return (
+        <button onFocus={handleFocus} onBlur={handleBlur}>Eyes On Me</button>
+    )
+}
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,19 @@
 // Code Keypad Component Here
+import React from "react";
 
 function Keypad (){
+
+
+    const handleChange=(event)=>{
+        console.log("ENTERING PASSWORD...", event.target.value)
+    };
+
     return (
-        <div></div>
-    )
+        <div>
+            <input type="password" onChange={handleChange}/>
+        </div>
+    );
 }
+
 
 export default Keypad;
